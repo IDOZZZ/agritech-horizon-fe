50c3952377ad3620abf559f445c15173dd48acd5
iqbal <iqbal.rosyidi.32@gmail.com>
2025-06-12 10:28:05 +0700

    fix testimonial

--- components/landing-page/SocialProof.tsx
+++ components/landing-page/SocialProof.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SocialProof = () => {
   return (
-    <div className="flex p-10 bg-white justify-center items-center gap-5">
+    <div className="flex flex-wrap p-10 bg-white justify-center items-center gap-5">
         <div className="px-6 py-10 flex flex-col justify-start gap-8">
             <div className="text-black text-6xl font-bold font-['Metropolis'] leading-20">400+</div>
             <div className="text-gray-600 text-lg font-normal font-['Manrope'] leading-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
